f7a83e98331e7d7f5bceffe6d2b600cb31e7b0cd
arkadiem <jasonusotelo@gmail.com>
2022-12-01 12:20:56 -0600

    Init

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 Route::prefix('/v1')->group(function () {
     Route::get('/hola', function () {
         return 'Hola mundo';
