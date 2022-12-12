6d3c89b2fe1e84fc4f81d389704334a1a34ab2fc
Hectorfrdz <110434536+Hectorfrdz@users.noreply.github.com>
2022-12-11 20:05:47 -0600

    Update verificarController.php

--- app/Http/Controllers/verificarController.php
+++ app/Http/Controllers/verificarController.php
@@ -33,11 +33,11 @@ class verificarController extends Controller
         $user->save();
 
         $response = Http::post('https://rest.nexmo.com/sms/json', [
-        "from"=>"Pablo Vazquez",
+        "from"=>"CoolerUTT",
         'api_key' => "a5f228dd",
         'api_secret' => "GRrWz6F07QPQ3g0z",
         'to' => 52 .$user->telefono,
-        'text' => "Tu codigo de verificacion es: ".$user->mobile_verify_code,
+        'text' => "Tu codigo de verificacion es: ".$user->verificationCode,
         ]);
         
         return View('espera');
