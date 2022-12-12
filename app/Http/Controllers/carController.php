5859fb7501cf54089d5de8205cc8bc84beb4e10c
Hectorfrdz <110434536+Hectorfrdz@users.noreply.github.com>
2022-12-12 10:02:14 -0600

    Update carController.php

--- app/Http/Controllers/carController.php
+++ app/Http/Controllers/carController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Car;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Validator;
 
 class carController extends Controller
